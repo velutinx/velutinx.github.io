06562adb482e4f4645e9232a0e5a63045500dfd8
velutinx <velutinx@yahoo.com>
2026-03-05 18:15:20 -0700

    Update paypal-sdk.js

--- functions/paypal-sdk.js
+++ functions/paypal-sdk.js
@@ -2,19 +2,19 @@ export async function onRequest(context) {
   const clientId = context.env.PAYPAL_CLIENT_ID;
 
   if (!clientId) {
-    return new Response(
-      'console.error("PAYPAL_CLIENT_ID not set in Cloudflare env vars");',
-      { headers: { "Content-Type": "application/javascript" } }
-    );
+    return new Response('console.error("PAYPAL_CLIENT_ID not set");', {
+      headers: { "Content-Type": "application/javascript" }
+    });
   }
 
   const cleanId = clientId.trim();
   const sdkUrl = "https://www.paypal.com/sdk/js?client-id=" + encodeURIComponent(cleanId) + "&currency=USD";
 
+  // Build JS code with safe concatenation
   const jsCode = [
     '(function(){',
     '  var s = document.createElement("script");',
-    '  s.src = "' + sdkUrl.replace(/"/g, '\\"') + '";',
+    '  s.src = "' + sdkUrl.replace(/"/g, '\\"').replace(/\\/g, '\\\\') + '";',
     '  s.async = true;',
     '  s.onload = function(){ console.log("PayPal SDK loaded"); };',
     '  s.onerror = function(e){ console.error("PayPal SDK load failed", e); };',
