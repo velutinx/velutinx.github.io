b030a38c68cfe5dbe41b056a300746f4024a2ff6
velutinx <velutinx@yahoo.com>
2026-03-22 00:04:50 -0700

    Update packs-data.js

--- assets/js/packs-data.js
+++ assets/js/packs-data.js
@@ -1,12 +1,12 @@
 // assets/js/packs-data.js
 export default [
   { id: "001", title: "[Pack 001] Tsunade - Naruto", category: 1, price: "PRICE_2", illustrationCount: 85 },
-  { id: "002", title: "[Pack 002] Astolfo - Fate/Grand Order", category: 2, price: "PRICE_2", illustrationCount: 154 },
-  { id: "003", title: "[Pack 003] Alexis Rhodes - Yu-Gi-Oh", category: 1, price: "PRICE_2", illustrationCount: 231 },
-  { id: "004", title: "[Pack 004] Nico Robin - One Piece", category: 1, price: "PRICE_2", illustrationCount: 222 },
-  { id: "005", title: "[Pack 005] MaoMao - Apothecary Diaries", category: 1, price: "PRICE_2", illustrationCount: 150 },
-  { id: "006", title: "[Pack 006] Rem - Re:Zero", category: 1, price: "PRICE_2", illustrationCount: 62 },
-  { id: "007", title: "[Pack 007] Narberal Gamma - Overlord", category: 1, price: "PRICE_2", illustrationCount: 54 },
+  { id: "002", title: "[Pack 002] Astolfo - Fate/Grand Order", category: 2, price: "PRICE_1", illustrationCount: 154 },
+  { id: "003", title: "[Pack 003] Alexis Rhodes - Yu-Gi-Oh", category: 1, price: "PRICE_3", illustrationCount: 231 },
+  { id: "004", title: "[Pack 004] Nico Robin - One Piece", category: 1, price: "PRICE_4", illustrationCount: 222 },
+  { id: "005", title: "[Pack 005] MaoMao - Apothecary Diaries", category: 1, price: "PRICE_5", illustrationCount: 150 },
+  { id: "006", title: "[Pack 006] Rem - Re:Zero", category: 1, price: "PRICE_6", illustrationCount: 62 },
+  { id: "007", title: "[Pack 007] Narberal Gamma - Overlord", category: 1, price: "PRICE_7", illustrationCount: 54 },
   { id: "008", title: "[Pack 008] Akeno Himejima - High School DxD", category: 1, price: "PRICE_2", illustrationCount: 86 },
   { id: "009", title: "[Pack 009] Gwen Stacy - Spider-Man", category: 1, price: "PRICE_2", illustrationCount: 46 },
   { id: "010", title: "[Pack 010] Nitocris - Fate/Grand Order", category: 1, price: "PRICE_2", illustrationCount: 116 }
